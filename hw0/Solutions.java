6e601b24fa31cfc9db54e108b4681fa87fdb0846
Tyler Le <tyql@berkeley.edu>
2022-01-24 16:47:14 -0800

    max function completed

--- hw0/Solutions.java
+++ hw0/Solutions.java
@@ -12,6 +12,16 @@ public class Solutions {
         return false;
     }
 
+    public static int max(int[] a) {
+        for (int curr = 0; curr < a; curr += 1) {
+            if ()
+        }
+    }
+
+    public static boolean wordBank(String word, String[] bank){
+        return false;
+    }
+
     // TODO: Fill in the method signatures for the other exercises
     // Your methods should be static for this HW. DO NOT worry about what this means.
     // Note that "static" is not necessarily a default, it just happens to be what

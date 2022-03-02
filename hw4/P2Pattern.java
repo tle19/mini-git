d5ae17829dc2557e48a3a51126a46bf16f07683b
Tyler Le <tyql@berkeley.edu>
2022-03-01 22:39:29 -0800

    finished hw4, but only did 3 of the patterns

--- hw4/P2Pattern.java
+++ hw4/P2Pattern.java
@@ -4,18 +4,18 @@
 
 public class P2Pattern {
     /* Pattern to match a valid date of the form MM/DD/YYYY. Eg: 9/22/2019 */
-    public static String P1 = "[0-2][0-9]?[/][0-3][0-9][2012]";
+    public static String P1 = "[0-2][0-9]?[/][0-3][0-9][2012]";//FIXME: Add your regex here
 
     /** Pattern to match 61b notation for literal IntLists. */
-    public static String P2 = ""; //FIXME: Add your regex here
+    public static String P2 = "\\(\\d+(\\,\\s+\\d+)*\\)";
 
     /* Pattern to match a valid domain name. Eg: www.support.facebook-login.com */
     public static String P3 = ""; //FIXME: Add your regex here
 
     /* Pattern to match a valid java variable name. Eg: _child13$ */
-    public static String P4 = ""; //FIXME: Add your regex here
+    public static String P4 = "[^0-9][a-zA-Z0-9$_]*";
 
     /* Pattern to match a valid IPv4 address. Eg: 127.0.0.1 */
-    public static String P5 = "";; //FIXME: Add your regex here
+    public static String P5 = "(\\d?\\d?[0-9]\\.){3}\\d?\\d?[0-9]";
 
 }

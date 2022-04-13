2dbdab8812731d88a06e874b882638f67770e54c
tle19 <tyle2003@gmail.com>
2022-04-12 23:18:19 -0700

    finished sum

--- hw7/Sum.java
+++ hw7/Sum.java
@@ -1,11 +1,17 @@
 /** HW #7, Two-sum problem.
- * @author
+ * @author Tyler Le
  */
 public class Sum {
 
     /** Returns true iff A[i]+B[j] = M for some i and j. */
     public static boolean sumsTo(int[] A, int[] B, int m) {
-        return false;  // REPLACE WITH YOUR ANSWER
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < B.length; j++) {
+                if (A[i] + B[j] == m) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
-
 }

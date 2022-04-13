dd20cf8408116aa38c58ccaa59d8a420cc56f0c8
tle19 <tyle2003@gmail.com>
2022-04-12 23:24:15 -0700

    finished again maybe?

--- hw7/Sum.java
+++ hw7/Sum.java
@@ -5,9 +5,12 @@ public class Sum {
 
     /** Returns true iff A[i]+B[j] = M for some i and j. */
     public static boolean sumsTo(int[] A, int[] B, int m) {
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < B.length; j++) {
-                if (A[i] + B[j] == m) {
+        if (A == null || B == null) {
+            return false;
+        }
+        for (int i: A) {
+            for (int j: B) {
+                if (i + j == m) {
                     return true;
                 }
             }
